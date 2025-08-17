d06b4b2abea4d74d3f182e1fff44113f34930b56
mumira-bit <mumiraesther41@gmail.com>
2025-08-17 22:28:24 +0300

    code challenge

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,109 @@
+class Author:
+    all = []
+
+    def __init__(self, name: str):
+        if not isinstance(name, str):
+            raise Exception("Author name must be a string.")
+        self.name = name
+        Author.all.append(self)
+
+    def articles(self):
+        return [article for article in Article.all if article.author == self]
+
+    def magazines(self):
+        return list({article.magazine for article in self.articles()})
+
+    def add_article(self, magazine, title):
+        if not isinstance(magazine, Magazine):
+            raise Exception("Magazine must be a Magazine instance.")
+        Article(self, magazine, title)
+
+
+class Magazine:
+    all = []
+
+    def __init__(self, name: str, category: str):
+        if not isinstance(name, str):
+            raise Exception("Magazine name must be a string.")
+        if not (2 <= len(name) <= 16):
+            raise Exception("Magazine name must be between 2 and 16 characters.")
+        self._name = name
+
+        if not isinstance(category, str):
+            raise Exception("Magazine category must be a string.")
+        if len(category) == 0:
+            raise Exception("Magazine category must not be empty.")
+        self._category = category
+
+        Magazine.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        if not isinstance(new_name, str):
+            raise Exception("Magazine name must be a string.")
+        if not (2 <= len(new_name) <= 16):
+            raise Exception("Magazine name must be between 2 and 16 characters.")
+        self._name = new_name
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, new_category):
+        if not isinstance(new_category, str):
+            raise Exception("Magazine category must be a string.")
+        if len(new_category) == 0:
+            raise Exception("Magazine category must not be empty.")
+        self._category = new_category
+
+    def articles(self):
+        return [article for article in Article.all if article.magazine == self]
+
+    def contributors(self):
+        contributors_list = [article.author for article in self.articles()]
+        unique_authors = []
+        for author in contributors_list:
+            if author not in unique_authors:
+                unique_authors.append(author)
+        return unique_authors
+
+    def article_titles(self):
+        articles = self.articles()
+        if not articles:
+            return None
+        return [article.title for article in articles]
+
+    def contributing_authors(self):
+        authors = self.contributors()
+        contributing = []
+        for author in authors:
+            count = sum(1 for article in self.articles() if article.author == author)
+            if count > 2:
+                contributing.append(author)
+        if contributing:
+            return contributing
+        return None
+
+
 class Article:
-    def __init__(self, author, magazine, title):
+    all = []
+
+    def __init__(self, author: Author, magazine: Magazine, title: str):
+        if not isinstance(author, Author):
+            raise Exception("Author must be an Author instance.")
+        if not isinstance(magazine, Magazine):
+            raise Exception("Magazine must be a Magazine instance.")
+        if not isinstance(title, str):
+            raise Exception("Article title must be a string.")
+
         self.author = author
         self.magazine = magazine
         self.title = title
-        
-class Author:
-    def __init__(self, name):
-        self.name = name
 
-    def articles(self):
-        pass
 
-    def magazines(self):
-        pass
-
-    def add_article(self, magazine, title):
-        pass
-
-    def topic_areas(self):
-        pass
-
-class Magazine:
-    def __init__(self, name, category):
-        self.name = name
-        self.category = category
-
-    def articles(self):
-        pass
-
-    def contributors(self):
-        pass
-
-    def article_titles(self):
-        pass
-
-    def contributing_authors(self):
-        pass
+        Article.all.append(self)
